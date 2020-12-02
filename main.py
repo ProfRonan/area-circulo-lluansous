5ca7479cf02f453326a8f6690bb77b596edb347c
lluansous <luansousa@protonmail.com>
2020-12-02 23:44:55 +0000

    Professor, está dando um erro e eu não sei como resolver.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 """Arquivo principal que será interpretado pelo interpretador."""
 # COLOQUE SEU CÓDIGO AQUI
+import math
+pi = 3.141592
 
 
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
+    raio = input("Digite o valor do raio:")
+    print('A área do círculo é: %.2f' % float(pi*(raio**2)))
+    
 
 
 if __name__ == '__main__':
