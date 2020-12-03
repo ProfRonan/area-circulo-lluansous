5604c7769b0238472ce87e682e1f4bce59136c83
lluansous <luansousa@protonmail.com>
2020-12-03 16:08:42 +0000

    Alterei a string do raio para float e deu certo, não tinha entendido muito bem, mas depois de algumas pesquisas eu consegui entender e solucionar.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 """Arquivo principal que será interpretado pelo interpretador."""
 # COLOQUE SEU CÓDIGO AQUI
 import math
-pi = 3.141592
 
 
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
-    raio = input("Digite o valor do raio:")
-    print('A área do círculo é: %.2f' % float(pi*(raio**2)))
+    raio = float (input("Digite o valor do raio:"))
+    print('A área do círculo é: %.2f' % float(math.pi*(raio*raio)))
+  
     
 
 
